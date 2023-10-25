ffe704bc5be748c9fad4991b453de53726541781
gsabury <gh.sabury@gmail.com>
2023-10-25 12:24:01 +0430

    Lesson-5

--- sandbox.ts
+++ sandbox.ts
@@ -1,39 +1,25 @@
-let character: string = 'mario';
-let age: number;
-let isLoggedIn: boolean;
+let age: any = 25;
 
-// age = 'luigi';
-age = 30;
+age = true;
+console.log(age);
 
-// isLoggedIn = 25;
-isLoggedIn = true;
 
-// arrays
-let ninjas: string[] = [];
+age = 'hello';
+console.log(age);
 
-ninjas.push('ryu');
-ninjas.push('chun-li');
-console.log(ninjas);
+age = { name: 'luigi' };
+console.log(age);
 
-// union types
-let mixed: (string|number|boolean)[] = [];
-mixed.push('hello');
+let mixed: any[] = [];
+mixed.push(5);
+mixed.push('mario');
 mixed.push(false);
-mixed.push(20);
 console.log(mixed);
 
-let uid: string|number;
-uid='123';
-uid = 123;
+let ninja: { name: any, age: any };
 
-// objects
-let ninjaOne: object;
-ninjaOne = { name: 'yoshi', age: 30 };
+ninja = { name: 'yoshi', age: 25 };
+console.log(ninja);
 
-let ninjaTwo: {
-  name: string,
-  age: number,
-  beltColour: string
-};
-
-ninjaTwo = { name: 'ken', age: 20, beltColour: 'black' };
+ninja = { name: 25, age: 'yoshi' };
+console.log(ninja);
