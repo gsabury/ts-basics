95cc58f39326afaf863608bd648c9d3b2d603901
gsabury <gh.sabury@gmail.com>
2023-10-25 12:12:59 +0430

    Lesson-4

--- sandbox.ts
+++ sandbox.ts
@@ -1,38 +1,39 @@
+let character: string = 'mario';
+let age: number;
+let isLoggedIn: boolean;
+
+// age = 'luigi';
+age = 30;
+
+// isLoggedIn = 25;
+isLoggedIn = true;
+
 // arrays
-let names = ['luigi', 'mario', 'yoshi'];
-// names.push('toad');
-// names.push(3);
-// names[1] = 3;
+let ninjas: string[] = [];
 
-let numbers = [10, 20, 12, 15];
+ninjas.push('ryu');
+ninjas.push('chun-li');
+console.log(ninjas);
 
-numbers.push(25);
-// numbers.push('shaun');
-// numbers[0] = 'shaun';
+// union types
+let mixed: (string|number|boolean)[] = [];
+mixed.push('hello');
+mixed.push(false);
+mixed.push(20);
+console.log(mixed);
 
-let mixed = ['ken', 4, 'chun-li', 8, 9];
-
-mixed.push('ryu');
-mixed.push(10);
-mixed[0] = 3;
+let uid: string|number;
+uid='123';
+uid = 123;
 
 // objects
-let ninja = {
-    name: 'mario',
-    belt: 'black',
-    age: 30
-  };
-  
-  ninja.age = 40;
-  ninja.name = 'ryu';
-  ninja.age = 45;
-  // ninja.skills = ['fighting', 'sneaking']
+let ninjaOne: object;
+ninjaOne = { name: 'yoshi', age: 30 };
 
-  ninja = {
-    name: 'yoshi',
-    belt: 'orange',
-    age: 40,
-    // skills: ['running'],
-  };
+let ninjaTwo: {
+  name: string,
+  age: number,
+  beltColour: string
+};
 
-
+ninjaTwo = { name: 'ken', age: 20, beltColour: 'black' };
