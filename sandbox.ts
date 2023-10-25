edbc8d792e63a206fbe64bc012310aa1e025a409
gsabury <gh.sabury@gmail.com>
2023-10-25 11:12:15 +0430

    lesson-3

--- sandbox.ts
+++ sandbox.ts
@@ -1,19 +1,38 @@
-let character = 'mario';
-let age = 30;
-let isBlackBelt = false;
+// arrays
+let names = ['luigi', 'mario', 'yoshi'];
+// names.push('toad');
+// names.push(3);
+// names[1] = 3;
 
-// character = 20;
-character = 'luigi';
+let numbers = [10, 20, 12, 15];
 
-// age = 'yoshi';
-age = 40;
+numbers.push(25);
+// numbers.push('shaun');
+// numbers[0] = 'shaun';
 
-// isBlackBelt = 'yes';
-isBlackBelt = true;
+let mixed = ['ken', 4, 'chun-li', 8, 9];
 
-const circle = (daiameter: number) => {
-    return daiameter * Math.PI;
-}
+mixed.push('ryu');
+mixed.push(10);
+mixed[0] = 3;
 
-// console.log(circle('hello'));
-console.log(circle(7.5));
+// objects
+let ninja = {
+    name: 'mario',
+    belt: 'black',
+    age: 30
+  };
+  
+  ninja.age = 40;
+  ninja.name = 'ryu';
+  ninja.age = 45;
+  // ninja.skills = ['fighting', 'sneaking']
+
+  ninja = {
+    name: 'yoshi',
+    belt: 'orange',
+    age: 40,
+    // skills: ['running'],
+  };
+
+
