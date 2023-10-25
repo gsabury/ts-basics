b815592a9fa646eefce6cb4092202cec7931d386
gsabury <gh.sabury@gmail.com>
2023-10-25 16:06:26 +0430

    Lesson-9

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,10 +1,29 @@
 "use strict";
-const logDetails = (uid, item) => {
-    console.log(`${item} has a uid of ${uid}`);
+// example 1
+// Signature
+let greet;
+// Function
+greet = (name, greeting) => {
+    console.log(`${name} says ${greeting}`);
 };
-logDetails(12345, "TV");
-const greet = (user) => {
-    console.log(`${user.name} say hello`);
+greet("Abdul Ghafor Sabury", "Hello");
+greet("John Doe", "Good Bye");
+// example 2
+let calc;
+calc = (numOne, numTwo, action) => {
+    if (action === 'add') {
+        return numOne + numTwo;
+    }
+    else {
+        return numOne - numTwo;
+    }
 };
-greet({ name: "Ghafor Sabury", uid: 7875097654 });
-greet({ name: "Mohammad Karimi", uid: '9097' });
+console.log(calc(4, 5, 'add'));
+console.log(calc(40, 5, 'minus'));
+// example 3
+let logDetails;
+logDetails = (ninja) => {
+    console.log(`${ninja.name} is ${ninja.age} years old`);
+};
+logDetails({ name: "Ali", age: 30 });
+logDetails({ name: "Fatema", age: 21 });
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,19 +1,36 @@
-type StringOrNumber = string | number;
+// example 1
 
-type ObjWithName = {
-    name: string,
-    uid: StringOrNumber
+// Signature
+let greet: (a: string, b: string) => void;
+
+// Function
+greet = (name: string, greeting: string) => {
+    console.log(`${name} says ${greeting}`);
 }
 
-const logDetails = (uid: StringOrNumber, item: string) => {
-    console.log(`${item} has a uid of ${uid}`);
+greet("Abdul Ghafor Sabury", "Hello");
+greet("John Doe", "Good Bye");
+
+// example 2
+let calc: (a: number, b: number, c: string) => number;
+
+calc = (numOne: number, numTwo: number, action: string) => {
+    if (action === 'add') {
+        return numOne + numTwo;
+    } else {
+        return numOne - numTwo;
+    }
 }
 
-logDetails(12345, "TV");
+console.log(calc(4, 5, 'add'));
+console.log(calc(40, 5, 'minus'));
 
-const greet = (user: ObjWithName) => {
-    console.log(`${user.name} say hello`);
+// example 3
+let logDetails: (obj: {name: string, age: number}) => void;
+
+logDetails = (ninja: {name: string, age: number}) => {
+  console.log(`${ninja.name} is ${ninja.age} years old`);
 }
 
-greet({ name: "Ghafor Sabury", uid: 7875097654 });
-greet({ name: "Mohammad Karimi", uid: '9097' });
+logDetails({name:"Ali", age: 30});
+logDetails({name:"Fatema", age: 21});
