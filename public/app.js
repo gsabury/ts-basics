49178b67c15ff3c0c25812534c878dfffc391709
gsabury <gh.sabury@gmail.com>
2023-10-26 17:03:57 +0430

    Generics

--- public/app.js
+++ public/app.js
@@ -21,3 +21,14 @@ form.addEventListener('submit', (e) => {
     }
     list.render(doc, type.value, 'end');
 });
+const docThree = {
+    uid: 1,
+    resourceName: 'person',
+    data: { name: 'shaun' }
+};
+const docFour = {
+    uid: 1,
+    resourceName: 'shoppingList',
+    data: ['bread', 'milk']
+};
+console.log(docThree, docFour);
--- src/app.ts
+++ src/app.ts
@@ -16,15 +16,62 @@ const ul = document.querySelector('ul')!;
 const list = new ListTemplate(ul);
 
 form.addEventListener('submit', (e: Event) => {
-  e.preventDefault();
+    e.preventDefault();
 
-  let doc: HasFormatter;
-  if (type.value === 'invoice') {
-    doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber);
-  } else {
-    doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
-  }
-  
-  list.render(doc, type.value, 'end');
-  
+    let doc: HasFormatter;
+    if (type.value === 'invoice') {
+        doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber);
+    } else {
+        doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
+    }
+
+    list.render(doc, type.value, 'end');
+
 });
+
+
+// GENERICS
+
+// const addUID = (obj: object) => {
+//   let uid = Math.floor(Math.random() * 100);
+//   return {...obj, uid};
+// }
+
+
+// const addUID = <T extends object>(obj: T) => {
+//     let uid = Math.floor(Math.random() * 100);
+//     return { ...obj, uid };
+// }
+
+// const addUID = <T extends { name: string }>(obj: T) => {
+//     let uid = Math.floor(Math.random() * 100);
+//     return { ...obj, uid };
+// }
+
+// let docOne = addUID({ name: 'yoshi', age: 40 });
+// docOne.name = "Sabury";
+// console.log(docOne);
+// let docTwo = addUID({name:'dfjdf'});
+
+// console.log(docOne.name);
+
+// with interfaces
+interface Resource<T> {
+    uid: number;
+    resourceName: string;
+    data: T;
+}
+
+const docThree: Resource<object> = {
+    uid: 1,
+    resourceName: 'person',
+    data: { name: 'shaun' }
+};
+
+const docFour: Resource<string[]> = {
+    uid: 1,
+    resourceName: 'shoppingList',
+    data: ['bread', 'milk']
+};
+
+console.log(docThree, docFour);
