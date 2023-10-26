c9f460980aeb09f1f22537a13171ad6159eb924b
gsabury <gh.sabury@gmail.com>
2023-10-26 11:26:57 +0430

    Lesson-11

--- public/app.js
+++ public/app.js
@@ -5,15 +5,22 @@ var anchor = document.querySelector('a');
 // }
 // console.log(anchor.href);
 //const form = document.querySelector('form')!;
-var form = document.querySelector('.new-item-form');
-// console.log(form.children);
-// inputs
-var type = document.querySelector('#type');
-var tofrom = document.querySelector('#tofrom');
-var details = document.querySelector('#details');
-var amount = document.querySelector('#amount');
-// Submit form
-form.addEventListener('submit', function (e) {
-    e.preventDefault();
-    console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
+// classes
+class Invoice {
+    constructor(c, d, a) {
+        this.client = c;
+        this.details = d;
+        this.amount = a;
+    }
+    format() {
+        return `${this.client} owes £${this.amount} for ${this.details}`;
+    }
+}
+const invOne = new Invoice('mario', 'work on the mario website', 250);
+const invTwo = new Invoice('luigi', 'work on the luigi website', 300);
+let invoices = [];
+invoices.push(invOne);
+invoices.push(invTwo);
+invoices.forEach(inv => {
+    console.log(`${inv.client}  ${inv.amount} ${inv.format()}`);
 });
--- src/app.ts
+++ src/app.ts
@@ -5,17 +5,49 @@ var anchor = document.querySelector('a') as HTMLAnchorElement;
 // }
 // console.log(anchor.href);
 //const form = document.querySelector('form')!;
-var form = document.querySelector('.new-item-form') as HTMLFormElement;
+
+
+
+// classes
+class Invoice {
+    readonly client: string;
+    private details: string;
+    public amount: number;
+
+    constructor(c: string, d: string, a: number) {
+        this.client = c;
+        this.details = d;
+        this.amount = a;
+    }
+
+    format() {
+        return `${this.client} owes £${this.amount} for ${this.details}`;
+    }
+}
+
+const invOne = new Invoice('mario', 'work on the mario website', 250);
+const invTwo = new Invoice('luigi', 'work on the luigi website', 300);
+
+let invoices: Invoice[] = [];
+invoices.push(invOne)
+invoices.push(invTwo);
+
+invoices.forEach(inv => {
+    console.log(`${inv.client}  ${inv.amount} ${inv.format()}`);
+})
+
+
+// var form = document.querySelector('.new-item-form') as HTMLFormElement;
 // console.log(form.children);
 
 // inputs
-var type = document.querySelector('#type') as HTMLSelectElement;
-var tofrom = document.querySelector('#tofrom') as HTMLInputElement;
-var details = document.querySelector('#details') as HTMLInputElement;
-var amount = document.querySelector('#amount') as HTMLInputElement;
+// var type = document.querySelector('#type') as HTMLSelectElement;
+// var tofrom = document.querySelector('#tofrom') as HTMLInputElement;
+// var details = document.querySelector('#details') as HTMLInputElement;
+// var amount = document.querySelector('#amount') as HTMLInputElement;
 
 // Submit form
-form.addEventListener('submit', function (e) {
-    e.preventDefault();
-    console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
-});
+// form.addEventListener('submit', function (e) {
+//     e.preventDefault();
+//     console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
+// });
