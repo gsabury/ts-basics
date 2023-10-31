e28415a2431cc0ee0270a5f629e37bb722cd0ac3
gsabury <gh.sabury@gmail.com>
2023-10-31 17:03:25 +0430

    added main repot link

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # ts-basics
+# Tutorial repo
+https://github.com/iamshaunjp/typescript-tutorial
--- src/app.ts
+++ src/app.ts
@@ -51,3 +51,67 @@ form.addEventListener('submit', (e: Event) => {
 // printData({ name: "Ali", age: 20, phone: "0787509653" });
 // printData([1, 2, 3, 4]);
 // printData([1, 2, 3, "Book"]);
+
+// function getData<x, y>(v1: x, v2: y) {
+// return v1 + ' ' + v2;
+// }
+
+// console.log(getData<number, number>(32, 42));
+// console.log(getData<string, string>("Ali", "Good Bye!"));
+
+
+// Generic with interface
+// interface UserData<X, Y> {
+//     name: X;
+//     rollNo: Y;
+// }
+
+// // Teacher is an object
+// const teacher: UserData<string, number> = {
+//     name: "Kazim Hussaini",
+//     rollNo: 1,
+// }
+
+// console.log('Teacher: ', teacher);
+// teacher.name = "Hamid";
+// teacher.rollNo = 780;
+// console.log('Teacher updated: ', teacher);
+
+// const user: UserData<string, string> = {
+//     name: "Ghafor Sabury",
+//     rollNo: '00987'
+// }
+// console.log('User: ', user);
+// user.name = "Sayed Mohammad";
+// user.rollNo = '1200';
+// console.log('User Updated: ', user);
+
+
+// class GenericClass<T> {
+//     private _value: T | undefined;
+
+//     constructor(private name: string) { }
+
+//     // Set Value
+//     public setValue(value: T) {
+//         this._value = value;
+//     }
+
+//     // Return value
+//     // public getValue(): T | undefined {
+//     //     return this._value;
+//     // }
+
+//     // Print
+//     public toString(): string {
+//         return `${this.name}: ${this._value}`;
+//     }
+// }
+
+// let myNumber = new GenericClass<number>('My Number');
+// myNumber.setValue(10);
+// console.log(myNumber.toString()); 
+
+// let myString = new GenericClass<string>("My String");
+// myString.setValue("Hello");
+// console.log(myString.toString());
